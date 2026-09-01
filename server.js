4b5beac83eb0f134e98d0ac5b5b1e87b3e5326e8
NXT-HARSHIT <harshitkrdubey26@gmail.com>
2026-09-01 17:56:28 +0530

    Final

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get("/second", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "second.html"));
 });
 
+
+app.get("/madewithlove",(req,res)=>{
+    res.render("madewithlove")
+})
+
+
 // ================================
 // CATCH-ALL ROUTE
 // MUST BE LAST
